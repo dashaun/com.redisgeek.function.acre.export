cb36aaba11c3baba03134dc88dda25d54c836f27
DaShaun <dashaun@redislabs.com>
2021-06-17 09:21:34 -0500

    simplify inputs

--- src/main/java/com/redisgeek/function/acre/export/Export.java
+++ src/main/java/com/redisgeek/function/acre/export/Export.java
@@ -5,6 +5,7 @@ import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.identity.AzureAuthorityHosts;
 import com.azure.identity.EnvironmentCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
 import com.azure.resourcemanager.redisenterprise.RedisEnterpriseManager;
 import com.azure.resourcemanager.redisenterprise.models.Cluster;
 import com.azure.resourcemanager.redisenterprise.models.Database;
@@ -22,20 +23,17 @@ public class Export implements Function<Mono<Optional<String>>, Mono<String>> {
     @Value("${acre_id}")
     private String acre_id;
 
-    @Value("${rg_name}")
-    private String rg_name;
-
     @Value("${blobSas}")
     private String blobSas;
 
     @Value("${storageKey}")
     private String storageKey;
 
-    @Value("${storageName}")
-    private String storageName;
+    @Value("${storageAccountName}")
+    private String storageAccountName;
 
     @Value("${storageContainerName}")
-    private String storageContainer;
+    private String storageContainerName;
 
     public Mono<String> apply(Mono<Optional<String>> request) {
         try {
@@ -45,16 +43,20 @@ public class Export implements Function<Mono<Optional<String>>, Mono<String>> {
             AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
             RedisEnterpriseManager redisEnterpriseManager = RedisEnterpriseManager
                     .authenticate(credential, profile);
-
+            AzureResourceManager azure = AzureResourceManager
+                    .authenticate(credential, profile)
+                    .withDefaultSubscription();
+            String resourceGroupName = azure.genericResources().getById(acre_id).resourceGroupName();
             Cluster cluster = redisEnterpriseManager.redisEnterprises().getById(acre_id);
             Database database = redisEnterpriseManager.databases().getById(acre_id + "/databases/default");
-            String blobSasUri = String.format("https://%s.blob.core.windows.net/%s%s", storageName, storageContainer, blobSas);
+            String blobSasUri = String.format("https://%s.blob.core.windows.net/%s%s", storageAccountName, storageContainerName, blobSas);
             ExportClusterParameters exportClusterParameters =
                     new ExportClusterParameters().withSasUri(blobSasUri + ";" + storageKey);
             exportClusterParameters.validate();
+
             redisEnterpriseManager
                     .databases()
-                    .export(rg_name, cluster.name(), database.name(), exportClusterParameters);
+                    .export(resourceGroupName, cluster.name(), database.name(), exportClusterParameters);
             return Mono.just("Export Complete");
         } catch (Exception e) {
             return Mono.just(e.getMessage());
